53932f89b368f822dd04293027e9b49484579a86
bef4 <befacool@gmail.com>
2023-06-08 00:38:32 +0200

    change order

--- app.js
+++ app.js
@@ -26,10 +26,6 @@ const storage = multer.diskStorage({
     },
 });
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-    res.status(200).json("File has been uploaded =)")
-});
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
@@ -51,6 +47,10 @@ app.use("/api/posts", postRoute);
 const categoryRoute = require("./routes/categories.routes")
 app.use("/api/categories", categoryRoute);
 
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded =)")
+});
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
