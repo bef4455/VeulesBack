d2c014ffae68f39fd837a984d6e4f6c3c99244ce
bef4 <befacool@gmail.com>
2023-06-07 13:27:16 +0200

    add build setting

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const app = require("./app");
-const authRoute = require("./routes/auth.routes")
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
